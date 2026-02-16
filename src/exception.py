2098a275359f9067be72606480cf4c9307f5b8d4
gopitharunvemuri <vemuri.gopitharun@gmail.com>
2026-02-17 01:07:04 +0530

    added logging and exception

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+from logger import logging
+
+def get_error_message(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in the file [{0}] in line number [{1}] error message is [{2}]".format(
+        filename, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = get_error_message(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Exception is working fine")
+        raise CustomException(e, sys)
