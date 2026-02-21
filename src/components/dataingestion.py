a67e25085afb743f69bf3d694001664645bc940b
gopitharunvemuri <vemuri.gopitharun@gmail.com>
2026-02-21 17:56:13 +0530

    modeltrainer

--- src/components/dataingestion.py
+++ src/components/dataingestion.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from datatransformation import DataTransformation
+from modeltrainer import modeltrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -42,3 +43,6 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, preprocessor = data_transformation.initiate_datatarnsformation(train_path, test_path)
+
+    model_trainer = modeltrainer()
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import pickle
 import os
 import sys
 from src.exception import CustomException
+from sklearn.metrics import r2_score
+from src.logger import logging
 
 def save_object(filepath, obj):
     try:
@@ -10,5 +12,24 @@ def save_object(filepath, obj):
         os.makedirs(dirname, exist_ok=True)
         with open(filepath, "wb") as f:
             pickle.dump(obj, f)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_model(X_train, X_test, y_train, y_test, models):
+    try:
+        model_report = {}
+
+        for i in models.items():
+            d = {}
+            model = i[1]
+            name = i[0]
+            model.fit(X_train, y_train)
+            logging.info(f"The model is {name}")
+            logging.info(f"The model is {model}")
+            y_pred = model.predict(X_test)
+            score = r2_score(y_pred = y_pred, y_true=y_test)
+            model_report[name] = score
+
+        return model_report
     except Exception as e:
         raise CustomException(e, sys)
